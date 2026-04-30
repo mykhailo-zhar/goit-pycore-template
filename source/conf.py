1e687da6270c0e326cd4ef53245095895551f496
ihateb335 <zhar.misha@gmail.com>
2026-04-30 15:33:29 +0300

    Add sphynx

---
+++ source/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+from os import environ as os_environ
+
+project = os_environ.get("PROJECT_NAME", "Sample Project")
+author = os_environ.get("AUTHOR", "Sample Author")
+copyright = f"2026, {author}"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+extensions = ["sphinx.ext.autodoc"]
